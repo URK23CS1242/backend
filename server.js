7266cdb3416ffab226ac63e559bdc64efca332b4
URK23CS1242 <153479704+URK23CS1242@users.noreply.github.com>
2025-11-02 15:31:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -517,5 +517,5 @@ app.delete('/api/friends/:userId/:friendId', async (req, res) => {
 // =============================
 // 🚀 START SERVER
 // =============================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
